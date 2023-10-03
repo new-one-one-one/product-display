8b8ae1a4646eee0c77029abf37e82e61fea57075
PIYUSH OHRI <58629311+new-one-one-one@users.noreply.github.com>
2023-10-03 05:18:01 +0000

    api for fetching product data completed

--- server/src/controllers/productController.ts
+++ server/src/controllers/productController.ts
@@ -1,20 +1,23 @@
 import * as fs from "fs";
 import { logger } from "../utils/logger";
 import { IProduct, products } from "../models/productModel";
-import path from "path";
 
-const dataFilePath = "../../data/products/list.json";
+const dataFilePath = __dirname + "/../../data/products/list.json";
+
+/**
+ * @function loadProductsData
+ *
+ * This function load the product data one time on starting the
+ * application.
+ */
 
 export const loadProductsData = () => {
   try {
-    const jsonPath = path.join(__dirname);
-    console.log("__dirname", __dirname);
     const rawData = fs.readFileSync(dataFilePath, "utf-8");
-    // @ts-ignore
     const jsonData: Array<IProduct> = JSON.parse(rawData);
-    console.log({ jsonData });
     products.push(...jsonData);
     logger.info("Product data loaded successfully.");
+    logger.info(`Number of product records loaded ${products.length}`);
   } catch (error: any) {
     logger.error("Error loading product data:", error);
   }
